3adab7785cad1a3f016095757cd92f40669c84bc
kamos <103331687+kamosqq@users.noreply.github.com>
2024-01-18 01:36:02 +0100

    testing for my uni
    okoknotwniking

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1705537718,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tlolabirynt-sheet0.png",
+		"images/gracz-sheet0.png",
+		"images/tlohud.png",
+		"images/bariera-sheet0.png",
+		"images/rr-sheet0.png",
+		"images/zycko1-sheet0.png",
+		"images/pustezycko-sheet0.png",
+		"images/koniecgry-sheet0.png",
+		"images/bramamety.png",
+		"images/ekrankoncowy.png",
+		"images/sprite3-sheet0.png",
+		"images/koniecgrystrefa.png",
+		"media/megamuza.ogg",
+		"media/obrazenia.ogg",
+		"media/podniesieniepkt.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
